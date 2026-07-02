e44e765823e9bf545cb6c92456b185d433800209
paulnorthrop <p.northrop@ucl.ac.uk>
2026-07-02 18:06:34 +0100

    Document x

--- R/summary_method.R
+++ R/summary_method.R
@@ -4,6 +4,7 @@
 #'
 #' @param object An object inheriting from class `"html2excel"` returned from
 #'   [`html2excel`].
+#' @param x An object returned by `summary.html2excel`.
 #' @param ... Further arguments to be passed to [`print.default`].
 #' @details The default `print` method for a list of tibbles prints the
 #'   dimensions of the tibbles and up to 10 rows and 6 columns of each tibble.
@@ -26,7 +27,7 @@ summary.html2excel <- function(object, ...) {
     return(X = lapply(object[[i]], FUN = dim))
   }
   table_dimensions <- lapply(X = 1:length(object), FUN = dim_fn)
-  names(table_dimensions) <- names(x)
+  names(table_dimensions) <- names(object)
   class(table_dimensions) <- "summary.evmissing"
   return(table_dimensions)
 }
